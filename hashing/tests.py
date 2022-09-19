7fd34606fec31de622f822136837713cd774754f
djag477 <101579022+djag477@users.noreply.github.com>
2022-09-19 21:16:09 +0200

    .

--- hashing/tests.py
+++ hashing/tests.py
@@ -3,7 +3,14 @@ from django.test import TestCase
 import time
 
 
-class FunctionalTestCase(TestCase):
+class UnitTestCase():
+    pass
+
+
+
+
+
+""" class FunctionalTestCase(TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
     def tearDown(self):
@@ -14,6 +21,14 @@ class FunctionalTestCase(TestCase):
         time.sleep(1)
         assert self.browser.page_source.find("Enter hash here")
 
+    def test_hash_of_hello(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name("submit").click()
+        self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
+ """
 
 
 
@@ -23,10 +38,6 @@ class FunctionalTestCase(TestCase):
 
 
 
-
-
-class UnitTestCase():
-    pass
 
 
 
