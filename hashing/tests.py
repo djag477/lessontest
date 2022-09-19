48e7e5d721cc517e427dc4df3e4b140ed7fc0af7
Rafe Stefano <stefanorafe@gmail.com>
2022-09-19 20:54:04 +0200

    first commit

---
+++ hashing/tests.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from django.test import TestCase
+import time
+
+
+class FunctionalTestCase(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_homepage(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        time.sleep(1)
+        assert self.browser.page_source.find("Enter hash here")
+
+
+
+
+
+
+
+
+
+
+
+
+class UnitTestCase():
+    pass
+
+
+
+
+
+
